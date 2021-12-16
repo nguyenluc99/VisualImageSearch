27d4c7e734a0ba7d8a8884eeee69929c9fdda24b
quyenld9699 <quyenld9699@gmail.com>
2021-12-16 19:55:03 +0700

    update UI

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 function App() {
     return (
         <Container className="App">
-            <h1>em chao anh quyen</h1>
-            <Button>Hello</Button>
+            <Header />
         </Container>
     );
 }
