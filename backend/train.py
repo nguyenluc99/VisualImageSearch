05e27ddb76ec73774e05a163f99fd872a5d27c59
thuando <thuan14121999@gmail.com>
2021-12-20 11:37:03 +0700

    feat: add basic visual search

---
+++ backend/train.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import numpy as np
+from PIL import Image
+
+from feature_extractor import FeatureExtractor
+
+if __name__ == '__main__':
+    fe = FeatureExtractor()
+
+    for img_path in sorted(Path("./images").glob("*.jpg")):
+        print(img_path)  # e.g., ./static/img/xxx.jpg
+        feature = fe.extract(img=Image.open(img_path))
+        feature_path = Path("./feature") / (img_path.stem + ".npy")  # e.g., ./static/feature/xxx.npy
+        np.save(feature_path, feature)
